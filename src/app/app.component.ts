43f55bb2c712d1d5ba26bec53ba7fcc809eb646b
cqgong <cgong@ebi.ac.uk>
2024-12-18 10:43:17 +0000

    :art: Restructure icons and add variables to fill color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,79 +1,36 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DomSanitizer} from "@angular/platform-browser";
 import {MatIconRegistry} from "@angular/material/icon";
+import {IconService} from "./services/icon.service";
 
 @Component({
   selector: 'cr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PathwayBrowser'
 
-  leftPanelIcons = [
-    {name: 'species', route: 'species-icon'},
-    {name: 'overlay', route: 'overlay-icon'},
-    {name: 'arrow-down', route: 'arrow-down'},
-    {name: 'arrow-right', route: 'arrow-right'},
-    {name: 'pathway-ehld', route: 'pathway-ehld'},
-    {name: 'pathway', route: 'pathway'},
-    {name: 'reaction', route: 'reaction'},
-    {name: 'transition', route: 'transition'},
-    {name: 'home', route: 'home'}
-  ];
-
-  species = [
-    {name: '9913', route: 'bos-taurus'},
-    {name: '6239', route: 'caenorhabditis-elegans'},
-    {name: '9615', route: 'canis-familiaris'},
-    {name: '7955', route: 'danio-rerio'},
-    {name: '44689', route: 'dictyostelium-discoideum'},
-    {name: '7227', route: 'drosophila-melanogaster'},
-    {name: '9031', route: 'gallus-gallus'},
-    {name: '9606', route: 'homo-sapiens'},
-    {name: '10090', route: 'mus-musculus'},
-    {name: '1773', route: 'mycobacterium-tuberculosis'},
-    {name: '5833', route: 'plasmodium-falciparum'},
-    {name: '10116', route: 'rattus-norvegicus'},
-    {name: '4932', route: 'saccharomyces-cerevisiae'},
-    {name: '4896', route: 'schizosaccharomyces-pombe'},
-    {name: '9823', route: 'sus-scrofa'},
-    {name: '8364', route: 'xenopus-tropicalis'}
-  ]
-
-  detailsPanelIcons = [
-    {name: 'details-tab', route: 'details-tab'},
-    {name: 'molecule-tab', route: 'molecule-tab'},
-    {name: 'results-tab', route: 'results-tab'},
-    {name: 'expression-tab', route: 'expression-tab'},
-    {name: 'info-tab', route: 'info-tab'},
-    {name: 'download-tab', route: 'download-tab'},
-    {name: 'double-arrow-right', route: 'double-arrow-right'},
-    {name: 'protein', route: 'protein'},
-    {name: 'complex', route: 'complex'},
-    {name: 'small-molecule', route: 'small-molecule'},
-    {name: 'defined-set', route: 'defined-set'},
-    {name: 'other-entity', route: 'other-entity'},
-    {name: 'polymer', route: 'polymer'},
-  ];
-
-  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
-    this.loadIcons();
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer, private iconService: IconService) {
   }
 
-  loadIcons(): void {
-    this.leftPanelIcons.forEach(icon => {
-      this.matIconRegistry.addSvgIcon(icon.name, this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/left-panel/${icon.route}.svg`));
-    });
 
-    this.species.forEach(icon => {
+  ngOnInit(): void {
+
+    const speciesIcon = this.iconService.getSpeciesIcons();
+    const actionIcons = this.iconService.getActionIcons();
+    const objectIcons = this.iconService.getObjectIcons();
+
+    speciesIcon.forEach(icon => {
       this.matIconRegistry.addSvgIcon(icon.name, this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/species/${icon.route}.svg`));
     });
 
-    this.detailsPanelIcons.forEach(icon => {
-      this.matIconRegistry.addSvgIcon(icon.name, this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/details-panel/${icon.route}.svg`));
+    actionIcons.forEach(icon => {
+      this.matIconRegistry.addSvgIcon(icon.name, this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/action-icons/${icon.route}.svg`));
     });
 
+    Object.values(objectIcons).forEach((icon) => {
+      this.matIconRegistry.addSvgIcon(icon.name, this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/object-icons/${icon.route}.svg`));
+    });
   }
-
 }
