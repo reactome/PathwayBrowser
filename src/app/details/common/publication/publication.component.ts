68478a4be3588a04961afbdceef361321793f349
cqgong <cgong@ebi.ac.uk>
2025-02-05 14:14:05 +0000

    :lipstick: Add font style and remove unused method

--- src/app/details/common/publication/publication.component.ts
+++ src/app/details/common/publication/publication.component.ts
@@ -1,5 +1,4 @@
 import {Component, input} from '@angular/core';
-import {environment} from "../../../../environments/environment";
 import {LiteratureReference} from "../../../model/graph/publication/literature-reference.model";
 
 @Component({
@@ -13,14 +12,8 @@ export class PublicationComponent{
   readonly showYear = input<boolean>(false);
   isExpanded = false;
 
-  environment = environment.host
 
   toggleAuthors() {
     this.isExpanded = !this.isExpanded;
-  }
-
-  openPersonDetails(dbId :number) {
-    const url = `${environment.host}/content/detail/person/${dbId}`;
-    window.open(url, '_blank');
   }
 }
