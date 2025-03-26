15f36c5645fb72f60645831758a24e7b4c06cec5
cqgong <cgong@ebi.ac.uk>
2025-03-26 15:52:56 +0000

    :wastebasket: Deprecate code that needs to be cleaned up

--- src/app/details/common/modification/modification.component.ts
+++ src/app/details/common/modification/modification.component.ts
@@ -1,7 +1,11 @@
 import {Component, input} from '@angular/core';
 import {Relationship} from "../../../model/graph/relationship.model";
+import {hasProperty, isEntity, isReferenceGroup} from "../../../services/utils";
+import {DatabaseObject} from "../../../model/graph/database-object.model";
+import {IconService} from "../../../services/icon.service";
 import HasModifiedResidue = Relationship.HasModifiedResidue;
-import {hasModification, isEntity, isReferenceGroup} from "../../../services/utils";
+import {DatabaseObjectService} from "../../../services/database-object.service";
+
 
 @Component({
   selector: 'cr-modification',
@@ -13,7 +17,29 @@ export class ModificationComponent {
 
   modifications = input.required<HasModifiedResidue[]>({alias: 'modifications'})
 
+
+  constructor(private iconService: IconService) {
+  }
+
+  getSymbol(obj: DatabaseObject) {
+    return this.iconService.getIconDetails(obj);
+  }
+
+  getModification(element: DatabaseObject) {
+    if (hasProperty(element, 'modification') && element.modification) {
+      const mod = element.modification as DatabaseObject;
+      if (isReferenceGroup(mod)) {
+        return mod;
+      }
+      if (isEntity(mod)) {
+        return mod;
+      }
+    }
+    return null;
+  }
+
   protected readonly isReferenceGroup = isReferenceGroup;
   protected readonly isEntity = isEntity;
-  protected readonly hasModification = hasModification;
+  protected readonly hasProperty = hasProperty;
+  protected readonly DatabaseObjectService = DatabaseObjectService;
 }
