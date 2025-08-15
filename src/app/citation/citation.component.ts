50f00785a15de74ee950cd81912af2b146d09a3f
cqgong <cgong@ebi.ac.uk>
2025-08-15 09:28:49 +0100

    :sparkles: copyToClipboard and mailto feature on citation dialog

--- src/app/citation/citation.component.ts
+++ src/app/citation/citation.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, inject} from '@angular/core';
+import {Component, computed, inject, signal} from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialogActions,
@@ -8,8 +8,10 @@ import {
 } from "@angular/material/dialog";
 import {Citation, CitationService} from "../services/citation.service";
 import {DownloadButtonComponent} from "../details/tabs/download-tab/download-button/download-button.component";
-import {MatIconButton} from "@angular/material/button";
+import {MatAnchor, MatButton, MatIconButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
+import {CdkCopyToClipboard} from "@angular/cdk/clipboard";
+import {FormsModule} from "@angular/forms";
 
 
 @Component({
@@ -34,9 +36,12 @@ export class CitationComponent {
   id = computed(() => this.data.id());
   downloadItems = computed(() => this.data.downloadItems());
 
+  copyLabel = signal('Copy');
+  copyIcon = signal('content_copy');
+
 
   staticCitation = computed(() => {
-    return !this.citation.isPathwayCitation(this.citationContent()) ? this.citationContent() : null
+    return !this.citation.isPathwayCitation(this.citationContent()) ? this.citationContent() : null;
   });
 
   imageCitation = computed(() => {
@@ -49,7 +54,19 @@ export class CitationComponent {
     return this.citation.isPathwayCitation(content) ? content.pathwayCitation : null;
   });
 
+  citationToCopy = computed(() => {
+    if (this.staticCitation()) return this.staticCitation() as string ?? '';
+    const content = [];
+    if (this.pathwayCitation()) content.push(this.pathwayCitation());
+    if (this.imageCitation()) content.push(this.imageCitation());
+    return content.join('\n');
+  });
+
   constructor(public citation: CitationService) {
   }
 
+  onCopyClick() {
+    this.copyLabel.set('Copied');
+    this.copyIcon.set('done');
+  }
 }
