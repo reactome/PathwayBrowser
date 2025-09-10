b1559b2ecf79016737d7c32a08232ba6636615f0
Eliot Ragueneau <eragueneau@ebi.ac.uk>
2025-09-10 13:19:30 +0100

    :sparkles: Link GIF availability to having several samples

--- src/app/details/tabs/download-tab/download-tab.component.ts
+++ src/app/details/tabs/download-tab/download-tab.component.ts
@@ -102,8 +102,8 @@ export class DownloadTabComponent {
   diagramItems = computed<DiagramItem[]>(() => {
     const hasEHLD = this.ehld.hasEHLD();
     const filteredFormats = this.formats.filter(format => {
-      const allowGif = this.analysis.isGSA() || this.analysis.type() === "EXPRESSION";
       return (hasEHLD || format !== DownloadFormat.SVG) && (allowGif || format !== DownloadFormat.GIF);
+      const allowGif = this.analysis.samples().length > 1;
     });
 
     return filteredFormats.map(format => {
